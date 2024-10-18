0bd1821a59692d607c6359928ab2aa3a66a3160f
sdgraham <98325142+sdgraham@users.noreply.github.com>
2024-10-18 19:33:25 +0100

    Add project files.

---
+++ FlagsTest/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace FlagsTest
+{
+    partial class FrmFlags
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelFlag = new System.Windows.Forms.Panel();
+            this.btnFrance = new System.Windows.Forms.Button();
+            this.btnBelgium = new System.Windows.Forms.Button();
+            this.btnUkraine = new System.Windows.Forms.Button();
+            this.btnRome = new System.Windows.Forms.Button();
+            this.btnNorthumbria = new System.Windows.Forms.Button();
+            this.btnPoland = new System.Windows.Forms.Button();
+            this.btnGermany = new System.Windows.Forms.Button();
+            this.btnMauritius = new System.Windows.Forms.Button();
+            this.btnColombia = new System.Windows.Forms.Button();
+            this.btnLatvia = new System.Windows.Forms.Button();
+            this.btnGambia = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelFlag
+            // 
+            this.panelFlag.Location = new System.Drawing.Point(893, 188);
+            this.panelFlag.Name = "panelFlag";
+            this.panelFlag.Size = new System.Drawing.Size(600, 400);
+            this.panelFlag.TabIndex = 0;
+            // 
+            // btnFrance
+            // 
+            this.btnFrance.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnFrance.Location = new System.Drawing.Point(6, 21);
+            this.btnFrance.Name = "btnFrance";
+            this.btnFrance.Size = new System.Drawing.Size(108, 56);
+            this.btnFrance.TabIndex = 1;
+            this.btnFrance.Text = "France";
+            this.btnFrance.UseVisualStyleBackColor = false;
+            this.btnFrance.Click += new System.EventHandler(this.btnFrance_Click);
+            // 
+            // btnBelgium
+            // 
+            this.btnBelgium.Location = new System.Drawing.Point(7, 83);
+            this.btnBelgium.Name = "btnBelgium";
+            this.btnBelgium.Size = new System.Drawing.Size(107, 56);
+            this.btnBelgium.TabIndex = 2;
+            this.btnBelgium.Text = "Belgium";
+            this.btnBelgium.UseVisualStyleBackColor = true;
+            this.btnBelgium.Click += new System.EventHandler(this.btnBelgium_Click);
+            // 
+            // btnUkraine
+            // 
+            this.btnUkraine.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnUkraine.Location = new System.Drawing.Point(6, 270);
+            this.btnUkraine.Name = "btnUkraine";
+            this.btnUkraine.Size = new System.Drawing.Size(107, 56);
+            this.btnUkraine.TabIndex = 3;
+            this.btnUkraine.Text = "Ukraine";
+            this.btnUkraine.UseVisualStyleBackColor = false;
+            this.btnUkraine.Click += new System.EventHandler(this.btnUkraine_Click);
+            // 
+            // btnRome
+            // 
+            this.btnRome.Location = new System.Drawing.Point(7, 145);
+            this.btnRome.Name = "btnRome";
+            this.btnRome.Size = new System.Drawing.Size(107, 56);
+            this.btnRome.TabIndex = 4;
+            this.btnRome.Text = "Rome";
+            this.btnRome.UseVisualStyleBackColor = true;
+            this.btnRome.Click += new System.EventHandler(this.btnRome_Click);
+            // 
+            // btnNorthumbria
+            // 
+            this.btnNorthumbria.Location = new System.Drawing.Point(6, 207);
+            this.btnNorthumbria.Name = "btnNorthumbria";
+            this.btnNorthumbria.Size = new System.Drawing.Size(107, 56);
+            this.btnNorthumbria.TabIndex = 5;
+            this.btnNorthumbria.Text = "Northumbria";
+            this.btnNorthumbria.UseVisualStyleBackColor = true;
+            this.btnNorthumbria.Click += new System.EventHandler(this.btnNorthumbria_Click);
+            // 
+            // btnPoland
+            // 
+            this.btnPoland.Location = new System.Drawing.Point(7, 332);
+            this.btnPoland.Name = "btnPoland";
+            this.btnPoland.Size = new System.Drawing.Size(107, 56);
+            this.btnPoland.TabIndex = 6;
+            this.btnPoland.Text = "Poland";
+            this.btnPoland.UseVisualStyleBackColor = true;
+            this.btnPoland.Click += new System.EventHandler(this.btnPoland_Click);
+            // 
+            // btnGermany
+            // 
+            this.btnGermany.Location = new System.Drawing.Point(7, 396);
+            this.btnGermany.Name = "btnGermany";
+            this.btnGermany.Size = new System.Drawing.Size(107, 56);
+            this.btnGermany.TabIndex = 7;
+            this.btnGermany.Text = "Germany";
+            this.btnGermany.UseVisualStyleBackColor = true;
+            this.btnGermany.Click += new System.EventHandler(this.btnGermany_Click);
+            // 
+            // btnMauritius
+            // 
+            this.btnMauritius.Location = new System.Drawing.Point(7, 458);
+            this.btnMauritius.Name = "btnMauritius";
+            this.btnMauritius.Size = new System.Drawing.Size(107, 56);
+            this.btnMauritius.TabIndex = 8;
+            this.btnMauritius.Text = "Mauritius";
+            this.btnMauritius.UseVisualStyleBackColor = true;
+            this.btnMauritius.Click += new System.EventHandler(this.btnMauritius_Click);
+            // 
+            // btnColombia
+            // 
+            this.btnColombia.Location = new System.Drawing.Point(7, 520);
+            this.btnColombia.Name = "btnColombia";
+            this.btnColombia.Size = new System.Drawing.Size(107, 56);
+            this.btnColombia.TabIndex = 9;
+            this.btnColombia.Text = "Colombia";
+            this.btnColombia.UseVisualStyleBackColor = true;
+            this.btnColombia.Click += new System.EventHandler(this.btnColombia_Click);
+            // 
+            // btnLatvia
+            // 
+            this.btnLatvia.Location = new System.Drawing.Point(6, 582);
+            this.btnLatvia.Name = "btnLatvia";
+            this.btnLatvia.Size = new System.Drawing.Size(107, 56);
+            this.btnLatvia.TabIndex = 10;
+            this.btnLatvia.Text = "Latvia";
+            this.btnLatvia.UseVisualStyleBackColor = true;
+            this.btnLatvia.Click += new System.EventHandler(this.btnLatvia_Click);
+            // 
+            // btnGambia
+            // 
+            this.btnGambia.Location = new System.Drawing.Point(7, 644);
+            this.btnGambia.Name = "btnGambia";
+            this.btnGambia.Size = new System.Drawing.Size(107, 56);
+            this.btnGambia.TabIndex = 11;
+            this.btnGambia.Text = "Gambia";
+            this.btnGambia.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFrance);
+            this.groupBox1.Controls.Add(this.btnGambia);
+            this.groupBox1.Controls.Add(this.btnNorthumbria);
+            this.groupBox1.Controls.Add(this.btnBelgium);
+            this.groupBox1.Controls.Add(this.btnLatvia);
+            this.groupBox1.Controls.Add(this.btnRome);
+            this.groupBox1.Controls.Add(this.btnColombia);
+            this.groupBox1.Controls.Add(this.btnMauritius);
+            this.groupBox1.Controls.Add(this.btnUkraine);
+            this.groupBox1.Controls.Add(this.btnGermany);
+            this.groupBox1.Controls.Add(this.btnPoland);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(131, 719);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stripes";
+            // 
+            // FrmFlags
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1505, 746);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panelFlag);
+            this.Name = "FrmFlags";
+            this.Text = "Flags";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelFlag;
+        private System.Windows.Forms.Button btnFrance;
+        private System.Windows.Forms.Button btnBelgium;
+        private System.Windows.Forms.Button btnUkraine;
+        private System.Windows.Forms.Button btnRome;
+        private System.Windows.Forms.Button btnNorthumbria;
+        private System.Windows.Forms.Button btnPoland;
+        private System.Windows.Forms.Button btnGermany;
+        private System.Windows.Forms.Button btnMauritius;
+        private System.Windows.Forms.Button btnColombia;
+        private System.Windows.Forms.Button btnLatvia;
+        private System.Windows.Forms.Button btnGambia;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ FlagsTest/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlagsTest
+{
+    public partial class FrmFlags : Form
+    {
+        public FrmFlags()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * Button event handlers
+         */
+        private void btnFrance_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_France);
+        }
+
+        private void btnBelgium_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_Belgium);
+        }
+
+        private void btnRome_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_Rome);
+        }
+
+        private void btnNorthumbria_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_Northumbria);
+        }
+
+        private void btnUkraine_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_Ukraine);
+        }
+
+        private void btnPoland_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_Poland);
+        }
+
+        private void btnGermany_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_Germany);
+        }
+
+        private void btnMauritius_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_Mauritius);
+        }
+
+
+        private void btnColombia_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_Colombia);
+        }
+
+        private void btnLatvia_Click(object sender, EventArgs e)
+        {
+            panelFlag.Invalidate();
+            panelFlag.Paint += new PaintEventHandler(Panel1_Latvia);
+        }
+
+
+        /*
+         * Draw the Flag of France
+         */
+        private void Panel1_France(object sender, PaintEventArgs e)
+        {
+            // Get the graphics object to draw on the panel and clear anything there
+            Graphics g = e.Graphics;
+            g.Clear(panelFlag.BackColor);
+
+            // Dimensions for the blue, white and red rectangles (three equal sections)
+            int width = panelFlag.Width / 3;
+            int height = panelFlag.Height;
+
+            // Create brushes for the colours of the French flag
+            Brush blueBrush = new SolidBrush(Color.Blue);
+            Brush whiteBrush = new SolidBrush(Color.White);
+            Brush redBrush = new SolidBrush(Color.Red);
+
+            // Draw the three sections of the flag
+            // Arguments are: starting x-coord, y-coord, rectangle width, rectangle height
+            g.FillRectangle(blueBrush, 0, 0, width, height); // Left - Blue
+            g.FillRectangle(whiteBrush, width, 0, width, height); // Middle - White
+            g.FillRectangle(redBrush, 2 * width, 0, width, height); // Right - Red
+
+            // Clean up brushes
+            blueBrush.Dispose();
+            whiteBrush.Dispose();
+            redBrush.Dispose();
+        }
+
+        /*
+         * Draw the flag of Belgium
+         */
+        private void Panel1_Belgium(object sender, PaintEventArgs e)
+        {
+            // write your code to draw the Belgian flag here
+        }
+
+      
+        /*
+         * Draw the flag of Rome
+         */
+        private void Panel1_Rome(object sender, PaintEventArgs e)
+        {
+            // Add your code to draw the flag of Rome here
+        }
+
+
+        /*
+         * Draw the flag of Northumbria
+         */
+        private void Panel1_Northumbria(object sender, PaintEventArgs e)
+        {
+            // Add your code to draw the flag of Northumbria here
+        }
+
+
+        /*
+         * Draw the flag of Ukraine
+         */
+        private void Panel1_Ukraine(object sender, PaintEventArgs e)
+        {
+            // Get the graphics object to draw on the panel
+            Graphics g = e.Graphics;
+            g.Clear(panelFlag.BackColor);
+
+            // Set up the size of the flag (two equal sections)
+            int width = panelFlag.Width;
+            int height = panelFlag.Height / 2;
+
+            // Create brushes for the colors of the Ukranian flag
+            Brush blueBrush = new SolidBrush(Color.Blue);
+            Brush yellowBrush = new SolidBrush(Color.Yellow);
+
+            // Draw the two sections of the flag
+            // Arguments are: starting x-coord, y-coord, rectangle width, rectangle height
+            g.FillRectangle(blueBrush, 0, 0, width, height);  // Top - blue
+            g.FillRectangle(yellowBrush, 0, height, width, height); // Bottom - yellow
+
+            // Clean up brushes
+            blueBrush.Dispose();
+            yellowBrush.Dispose();
+        }
+
+
+        /*
+         * Draw the flag of Poland
+         */
+        private void Panel1_Poland(object sender, PaintEventArgs e)
+        {
+            // Add your code to draw the Polish flag here
+        }
+
+
+        /*
+         * Draw the flag of Germany
+         */
+        private void Panel1_Germany(object sender, PaintEventArgs e)
+        {
+            // write your code to draw the German flag here
+        }
+
+
+        /*
+         * Draw the flag of Mauritius
+         */
+        private void Panel1_Mauritius(object sender, PaintEventArgs e)
+        {
+            // write your code to draw the flag of Mauritius here
+        }
+
+        /*
+         * Draw the flag of Colombia
+         */
+        private void Panel1_Colombia(object sender, PaintEventArgs e)
+        {
+            // write your code to draw the Colombian flag here
+        }
+
+        /*
+         * Draw the flag of Latvia
+         */
+        private void Panel1_Latvia(object sender, PaintEventArgs e)
+        {
+            // write your code to draw the Latvian flag here
+        }
+
+        /*
+         * Draw the flag of Gambia
+         */
+        private void Panel1_Gambia(object sender, PaintEventArgs e)
+        {
+            // write your code to draw the Gambian flag here
+        }
+    }
+}
